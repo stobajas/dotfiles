cdf4ccf7b1dc29ef734bbe744e424478990f0f5b
vagrant <vagrant@vagrant.myshopify.io>
2016-01-22 18:44:31 +0000

    fix linux install

--- install.sh
+++ install.sh
@@ -7,10 +7,14 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/zenorocha/dracula-theme/
 ln -s $HOME/dracula-theme/zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
 
+rm $HOME/.zshrc
+rm $HOME/.vimrc
+rm $HOME/.gitconfig
+
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
 
 git clone git@github.com:powerline/fonts.git
-sh $HOME/font/install.sh
+sh $HOME/fonts/install.sh
 
